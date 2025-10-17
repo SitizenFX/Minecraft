f2406315d9be70518ad611225595edeb14553623
Shi-Lan <XdllQs@gmail.com>
2025-10-17 11:13:15 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,21 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Elijah Siita / VidllQ Authority'
-description 'Minecraft Bridge for FiveM'
+description 'Minecraft Bridge for FiveM / FiveZ'
 version '1.0.0'
 
+-- Client script: runs on player side
 client_scripts {
     'client.lua'
 }
 
+-- Server scripts: runs on server
 server_scripts {
-    '@oxmysql/lib/MySQL.lua', -- optional if using database
+    '@oxmysql/lib/MySQL.lua', -- optional if you use oxmysql database features
     'server.lua'
 }
 
+-- Optional dependency if using MySQL
 dependencies {
-    'oxmysql' -- optional
+    'oxmysql'
 }
