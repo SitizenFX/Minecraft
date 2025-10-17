0398ba6afae9206b5de04d2652531715e340b814
Shi-Lan <XdllQs@gmail.com>
2025-10-17 02:53:41 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Elijah SitizenFX'
+description 'Minecraft Bridge for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+dependencies {
+    'mysql-async'
+}
 
